9ac9736f87f257c0b98e389473acf6af62844b2f
Matheus Braun <matheeusbl@gmail.com>
2020-09-16 00:59:05 -0300

    fixing sort by name method

--- src/utils/sortArrayByName.js
+++ src/utils/sortArrayByName.js
@@ -1,8 +1,10 @@
 export default arrayToBeSorted => {
   return arrayToBeSorted.sort((currentDragon, nextDragon) => {
-    if (currentDragon.name > nextDragon.name) return 1;
+    if (currentDragon.name.toLowerCase() > nextDragon.name.toLowerCase())
+      return 1;
 
-    if (currentDragon.name < nextDragon.name) return -1;
+    if (currentDragon.name.toLowerCase() < nextDragon.name.toLowerCase())
+      return -1;
 
     return 0;
   });
